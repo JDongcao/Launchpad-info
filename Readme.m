198358affef7489b3bb89c033aade66858f3100b
JDongcao <34732261+JDongcao@users.noreply.github.com>
2019-06-28 16:04:03 +0800

    Create Readme.m

---
+++ Readme.m
@@ -0,0 +1,66 @@
+# The info about Launchpad
+百度百科介绍：
+Launchpad 是Ubuntu母公司Canonical有限公司所资助架设的网站，是一个提供维护、支持或连络Ubuntu开发者的平台。
+在 Launchpad 网站上可以进行 Ubuntu(或其相关软体)的翻译工作或管理开发专权，也可以利用该网站的汇报机制来报告相关软件的 Bug，或进一步提供相关建议。
+
+The Launchpad Help
+
+Launchpad is an open source suite of tools that help people and teams to work together on software projects. 
+See the tour for an introduction to what you can do with Launchpad.
+
+#One can...
+
+Track my project's bugs ; Host my project's code ;Translate my project ;Distribute Ubuntu software ;Run a mailing list
+Help translate a project
+
+#New to Launchpad
+
+Privacy, confidentiality and disclosure in Launchpad
+
+The types of information that Launchpad recognises, how to change information types, and how to share them with people.
+
+#User guide
+Your account
+Create a new account, learn about karma, log into other websites with OpenID, contact people and teams.
+
+Teams
+Join a team, create and run a team, set up a team mailing list.
+
+Projects
+Register a project, learn about releases, publish your project files.
+
+Bug tracker
+Bug tracking for your project, track one bug across many projects, Launchpad's bug statuses, subscribe to bugs by email and Atom feed, learn the bug tracker's email interface, Bugzilla and Trac plugins.
+
+Translations
+Help for translators, translation help for projects.
+
+Code Hosting
+Finding and downloading code, uploading code to Launchpad, team branches, code reviews, linking branches to bugs, non-project branches, importing code from other sites into Bazaar branches or Git repositories on Launchpad.
+
+Answers
+Ask for help with free software, offer help, use Answers for your project.
+
+Blueprint
+Track specifications and chunks of work.
+
+Packaging
+Personal package archives — build and publish Ubuntu packages in your own apt repository. Source package recipes — create daily builds from a Bazaar or Git branch. Common package upload errors.
+
+API
+Use the Python library, use the API without Python, read the reference documentation and more.
+
+#Get involved
+Launchpad is free software. You can get involved in a number of ways:
+    Join the development community: learn about and participate in Launchpad development.
+
+    Beta testing: be the first to try new features and help shape them before launch.
+
+    Documentation team: help others to use Launchpad with user guides, screencasts and more.
+
+
+
+https://launchpad.net/~cug2019
+
+
+
